1ab913f7c0e6dcc9f4fb93f96a219b06591081a2
Susan Choi <2539lee@gmail.com>
2022-03-22 23:28:20 -0700

    JS: Display balance, income & expense

--- script.js
+++ script.js
@@ -6,37 +6,59 @@ const form = document.getElementById('form')
 const text = document.getElementById('text')
 const amount = document.getElementById('amount')
 
-const dummyTransactions=[
-    {id:1, text: "Flower", amount: -20},
-    {id:2, text: "Salary", amount: 300},
-    {id:3, text: "Book", amount: -10},
-    {id:4, text: "Camera", amount: 150}
+const dummyTransactions = [
+  { id: 1, text: 'Flower', amount: -20 },
+  { id: 2, text: 'Salary', amount: 300 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 },
 ]
 
-let transactions = dummyTransactions;
+let transactions = dummyTransactions
 
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
- // Get sign
- const sign = transaction.amount < 0 ? '-' : '+'
+  // Get sign
+  const sign = transaction.amount < 0 ? '-' : '+'
 
- const item = document.createElement('li')
+  const item = document.createElement('li')
 
- // Add class based on value
- item.classList.add(transaction.amount < 0 ? 'minus' : 'plus')
+  // Add class based on value
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus')
 
- item.innerHTML = `
- ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class="delete-btn">x</button>
+  item.innerHTML = `
+ ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span> <button class="delete-btn">x</button>
  `
 
- list.appendChild(item)
+  list.appendChild(item)
+}
+
+// Update the balance, income and expense
+function updateValues() {
+  const amounts = transactions.map((transaction) => transaction.amount)
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2)
+
+  const expense =
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -(1).toFixed(2)
+  balance.innerText = `$${total}`
+  money_plus.innerText = `$${income}`
+  money_minus.innerText = `$${expense}`
 }
 
 // Init app
 function init() {
-    list.innerHTML = ''
+  list.innerHTML = ''
 
-    transactions.forEach(addTransactionDOM)
+  transactions.forEach(addTransactionDOM)
+  updateValues()
 }
 
 init()
