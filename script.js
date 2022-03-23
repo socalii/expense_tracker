a1b3e96558aad8c6650f34cf420b88eed13b35c1
Susan Choi <2539lee@gmail.com>
2022-03-22 23:48:03 -0700

    JS: Add & delete transactions

--- script.js
+++ script.js
@@ -15,6 +15,34 @@ const dummyTransactions = [
 
 let transactions = dummyTransactions
 
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault()
+
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please a text and amount')
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value,
+    }
+    transactions.push(transaction)
+
+    addTransactionDOM(transaction)
+
+    updateValues()
+
+    text.value = ''
+    amount.value = ''
+  }
+}
+
+// Generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 100000000)
+}
+
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
   // Get sign
@@ -28,7 +56,9 @@ function addTransactionDOM(transaction) {
   item.innerHTML = `
  ${transaction.text} <span>${sign}${Math.abs(
     transaction.amount
-  )}</span> <button class="delete-btn">x</button>
+  )}</span> <button class="delete-btn" onclick="removeTransaction(${
+    transaction.id
+  })">x</button>
  `
 
   list.appendChild(item)
@@ -45,12 +75,18 @@ function updateValues() {
     .reduce((acc, item) => (acc += item), 0)
     .toFixed(2)
 
-  const expense =
-    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
-    -(1).toFixed(2)
+  const expense = Math.abs(
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0)
+  ).toFixed(2)
   balance.innerText = `$${total}`
   money_plus.innerText = `$${income}`
   money_minus.innerText = `$${expense}`
+}
+
+// Remove transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter((transaction) => transaction.id !== id)
+  init()
 }
 
 // Init app
@@ -62,3 +98,5 @@ function init() {
 }
 
 init()
+
+form.addEventListener('submit', addTransaction)
